51a3439c4b6220aad1bfb85592ff48ecd805c330
Fabio Oesch <fabiooesch@hotmail.com>
2018-09-17 20:28:47 +0100

    main and tree

--- group13/main.py
+++ group13/main.py
@@ -1,5 +1,10 @@
-WIDTH = 300
-HEIGHT = 300
+tree = Actor('tree')
+tree.pos = 250, 0
+
+WIDTH = 500
+HEIGHT = 500
 
 def draw():
-    screen.fill((128, 0, 0))
+    screen.fill((255, 255, 255))
+    tree.draw()
+    
