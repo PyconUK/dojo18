6ed9520fab19978072f2d6f7a6fb5c872bbaa428
Alex Willmer <alex@moreati.org.uk>
2018-09-17 20:51:25 +0100

    First draft, cycle images every 3 seconds

---
+++ team2/team2.py
@@ -0,0 +1,19 @@
+import itertools
+import pathlib
+import time
+
+WIDTH = 1280
+HEIGHT = 800
+
+backgrounds = itertools.cycle(sorted((p.stem
+                                      for p in pathlib.Path('images').iterdir()),
+                                     key=lambda s: 'sp su au wi'.index(s[:2])
+                                     ))
+
+def draw():
+    background = next(backgrounds)
+    screen.clear()
+    screen.blit(background,(0, 0))
+
+def update():
+    time.sleep(3)
