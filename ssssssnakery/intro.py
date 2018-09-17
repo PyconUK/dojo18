c06e8a226a7802be1845f7910061297b05db81ec
Isioviel <sarah.townson@scienceoxford.com>
2018-09-17 20:27:09 +0100

    added snake dir

---
+++ ssssssnakery/intro.py
@@ -0,0 +1,28 @@
+alien = Actor('alien')
+alien.topright = 0,10
+
+WIDTH = 500
+HEIGHT = alien.height + 20
+
+def draw():
+    screen.clear()
+    alien.draw()
+    
+def update():
+    alien.left += 2
+    if alien.left > WIDTH:
+        alien.right = 0
+
+def on_mouse_down(pos):
+    if alien.collidepoint(pos):
+        set_alien_hurt()
+
+
+def set_alien_hurt():
+    alien.image = 'alien_hurt'
+    sounds.eep.play()
+    clock.schedule_unique(set_alien_normal, 1.0)
+
+
+def set_alien_normal():
+    alien.image = 'alien'
