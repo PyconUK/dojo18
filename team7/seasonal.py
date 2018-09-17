9a523fc5373bc906bcc16ac3889842980ee7a4b7
Madda <nobody.youknow@local>
2018-09-17 20:26:04 +0100

    Ugly hack to cycle through seasons

--- team7/seasonal.py
+++ team7/seasonal.py
@@ -1,5 +1,37 @@
+import time
+
 WIDTH = 600
 HEIGHT = 600
 
+SPRING = (0, 255, 0)
+SUMMER = (255, 255, 25)  # Laser lemon. Yes, really.
+AUTUMN = (255, 0, 0)
+WINTER = (255, 255, 255)
+YEAR_STARTED = False
+
+
 def draw():
-    screen.fill((128, 0, 0))
+    global YEAR_STARTED
+    if not YEAR_STARTED:
+       YEAR_STARTED = True
+       start_winter()
+
+
+def start_spring():
+    screen.fill(SPRING)
+    clock.schedule_unique(start_summer, 1)
+
+
+def start_summer():
+    screen.fill(SUMMER)
+    clock.schedule_unique(start_autumn, 1)
+
+
+def start_autumn():
+    screen.fill(AUTUMN)
+    # TODO: No, Nathaniel, no.
+
+
+def start_winter():
+    screen.fill(WINTER)
+    clock.schedule_unique(start_spring, 1)
